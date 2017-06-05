41ca9f4531774764d0323a0519200680e86e6d43
nelyflores <nely.afloresr@gmail.com>
2017-06-05 16:00:37 +0000

    modificando readme y jquery

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
  
   function findMe(){
-    var salida=document.getElementById('map');
+    var salida=$('#map');
   function localizacion(posicion){
     var latitude=posicion.coords.latitude;
     var longitude=posicion.coords.longitude;
     var imgURL="https://maps.googleapis.com/maps/api/staticmap?center="+
     latitude+","+longitude+"&size=600x300&markers=color:red%7C"+latitude+","+longitude+"&Key=AIzaSyD0SBG5F97yON9GcTOwudBMILlgKv4uTds";
-    salida.innerHTML="<img src='"+imgURL+"'>";
+    salida.html("<img src='"+imgURL+"'>");
   }
     navigator.geolocation.getCurrentPosition(localizacion);
-  }findMe();
+  };
+  $(document).ready(findMe);
